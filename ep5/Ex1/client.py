58057873d1028db6db780e0bea85bc412e6a211b
soph <sophiaceline@usp.br>
2023-12-13 21:10:15 -0300

    added client-server basic configs

--- ep5/Ex1/client.py
+++ ep5/Ex1/client.py
@@ -1,3 +1,32 @@
 # O cliente fará o papel de interface homem-máquina (IHM)
+# client.py
 
 from pymodbus.client.sync import ModbusTcpClient
+
+PORT1 = 5020
+PORT2 = 5021
+SLAVE_ADDRESSES = [0, 0]
+SERVER_ADDRESSES = [("localhost", PORT1), ("localhost", PORT2)]
+
+print("Endereços das solicitações de leitura: ", SLAVE_ADDRESSES)
+
+# Server 1
+client = ModbusTcpClient(SERVER_ADDRESSES[0][0], SERVER_ADDRESSES[0][1])
+client.connect()
+disc_inputs1 = client.read_discrete_inputs(SLAVE_ADDRESSES[0])
+input_regs1 = client.read_input_registers(SLAVE_ADDRESSES[0])
+print("Dados das entradas discretas do server com endereço " + str(SERVER_ADDRESSES[0][1]) + ": ", disc_inputs1)
+print("Dados dos registros de entradado server com endereço " + str(SERVER_ADDRESSES[0][1]) + ": ", input_regs1)
+client.close()
+
+# Server 2
+client2 = ModbusTcpClient(SERVER_ADDRESSES[1][0], SERVER_ADDRESSES[1][1])
+client2.connect()
+disc_inputs2 = client2.read_discrete_inputs(SLAVE_ADDRESSES[1])
+input_regs2 = client2.read_input_registers(SLAVE_ADDRESSES[1])
+print("Dados das entradas discretas do server com endereço " + str(SERVER_ADDRESSES[1][1]) + ": ", disc_inputs2)
+print("Dados dos registros de entradado server com endereço " + str(SERVER_ADDRESSES[1][1]) + ": ", input_regs2)
+
+print("Endereços dos servidores: ", SERVER_ADDRESSES)
+
+
--- ep5/Ex1/server.py
+++ ep5/Ex1/server.py
@@ -1,5 +1,42 @@
 # O servidor fará o papel de cada sensor da planta
-
-from pymodbus.server.sync import StartTcpServer
-from pymodbus.datastore import ModbusSequentialDataBlock
+# Entradas discretas - booleano
+# Registros de entrada - valores numéricos como palavras de 16 bits
+# server.py
 from pymodbus.datastore import ModbusServerContext, ModbusSlaveContext
+from pymodbus.server.sync import StartTcpServer
+from pymodbus.datastore import ModbusSequentialDataBlock, ModbusSparseDataBlock
+import threading
+
+DI_BLOCK1 = ModbusSequentialDataBlock(0, [0])  
+IR_BLOCK1 = ModbusSequentialDataBlock(0, [0])  
+DI_BLOCK2 = ModbusSequentialDataBlock(0, [0])  
+IR_BLOCK2 = ModbusSequentialDataBlock(0, [0])  
+print("Entrada discreta do server1:", DI_BLOCK1)
+print("Registros de entrada do server1:", IR_BLOCK1)
+print("Entrada discreta do server2:", DI_BLOCK2)
+print("Registros de entrada do server2:", IR_BLOCK2)
+
+PORT1 = 5020
+PORT2 = 5021
+slave1 = ModbusSlaveContext(di=DI_BLOCK1, ir=IR_BLOCK1, zero_mode=True)
+slave2 = ModbusSlaveContext(di=DI_BLOCK2, ir=IR_BLOCK2, zero_mode=True)
+
+context1 = ModbusServerContext(slaves=slave1, single=True)
+context2 = ModbusServerContext(slaves=slave2, single=True)
+
+def start_server(context, port):
+    StartTcpServer(context, address=("localhost", port))
+
+thread1 = threading.Thread(target=start_server, args=(context1, PORT1))
+thread2 = threading.Thread(target=start_server, args=(context2, PORT2))
+
+
+thread2.start()
+thread1.start()
+
+
+#StartTcpServer(context1, address=("localhost", PORT1))
+#StartTcpServer(context2, address=("localhost", PORT2))
+
+
+
