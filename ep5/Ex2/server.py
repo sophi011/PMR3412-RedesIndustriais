17e4353349d4ff79966fa4bfb574d87ee2f9127b
soph <sophiaceline@usp.br>
2023-12-14 22:13:28 -0300

    added ex2

---
+++ ep5/Ex2/server.py
@@ -0,0 +1,5 @@
+"""
+Os Registros de entrada de cada escravo devem armazenar os valores referentes 
+ao sensor de nível e as Entradas Discretas servirão para armazenar o estado da
+botoeira ativação da válvula de fluxo.
+"""
